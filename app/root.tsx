7c03326978897a2266287fc4c02ef72595caa600
DevTiger <77429319+ohwoo-kwon@users.noreply.github.com>
2025-06-17 09:42:41 +0900

    google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -56,6 +56,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HZS8YQ6SMS"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-HZS8YQ6SMS');
+        </script>
       </body>
     </html>
   );
